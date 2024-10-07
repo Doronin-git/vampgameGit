157ecb474e40d75d2ded8a82ef6d1b4568645e00
Doronin-git <doronin.forwork@gmail.com>
2024-10-07 06:57:24 +0200

    trying to make waves

--- game.gd
+++ game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var spawned_mobs = 0
 
 
 func spawn_mob():
@@ -7,9 +8,16 @@ func spawn_mob():
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
-
+	
+	#waves and mob stats
 func _on_timer_timeout():
 	spawn_mob()
+	spawned_mobs += 1
+	if spawned_mobs <= 30:
+		get_node("Player/Path2D/PathFollow2D/Timer").wait_time = 1
+		#make wave one signal
+		
+	
 
 
 

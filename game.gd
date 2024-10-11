7248559df3da0e1976a81ff43a5480399c49bb74
Doronin-git <doronin.forwork@gmail.com>
2024-10-11 23:12:10 +0200

    SCOREEEEEEEEEEE!!!!!!!!!!!!!!!!!!

--- game.gd
+++ game.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-
+var score = 0
+@onready var score_label = %ScoreLabel #update to show score
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
+	new_mob.connect("mob_died", Callable(self, "_on_mob_died"))  # conect mob y signal 
 
 func _on_timer_timeout():
 	spawn_mob()
 
-
+func _on_mob_died():
+	score += 1
+	score_label.text = str(score) # conect to label
 
 func _on_player_health_deplited():
 	%GameOver.visible = true
--- mob.gd
+++ mob.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var health = 2 
+signal mob_died
 
 @onready var player = get_node("/root/Game/Player")
 
@@ -10,13 +11,14 @@ func _ready():
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
-	move_and_slide()
+	move_and_slide()	
 	
 func take_damage():
 	health -= 1 
 	%Slime.play_hurt()
 	
 	if health == 0:
+		emit_signal("mob_died") 
 		queue_free()
 		
 		const SMOKE_EXPLOSION = preload("res://smoke_explosion/smoke_explosion.tscn")

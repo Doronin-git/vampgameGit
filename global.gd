fc3e0a0b2cd2a106d912ac763b2d9540f2257b65
Doronin-git <doronin.forwork@gmail.com>
2024-10-15 12:44:10 +0200

    trying to make shop

--- global.gd
+++ global.gd
@@ -1,9 +1,17 @@
 extends Node
 
 var score = 0
+
+#stats
 var player_speed = 600.0
 var player_damage = 15.0
 var player_attak_rate = 0.5 #todavia no sé como hacerlo
 var player_max_health = 100.0
 var mob_max_health = 30.0
 var mob_speed = 400.0
+
+#upgradescosts
+var speed_upgrade_cost = 100
+var hp_upgrade_cost = 100 
+var damage_upgrade_cost = 100
+var reload_upgrade_cost = 100
--- shop.gd
+++ shop.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var speed_upgrade_cost =
+var hp_upgrade_cost =
+var damage_upgrade_cost =
+var reload_upgrade_cost =
+
+func havemoney(upgrade_cost): 
+	if Global.score > upgrade_cost:
+		return true
+
 
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
@@ -7,3 +16,20 @@ func _on_restart_pressed():
 
 func _ready():
 	%Shop_money_label.text = str(Global.score)
+
+
+func _on_upgrade_speed_pressed() -> void:
+	pass
+	
+
+
+func _on_upgrade_hp_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_upgrade_damage_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_upgrade_reload_pressed() -> void:
+	pass # Replace with function body.
